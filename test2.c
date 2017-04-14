aceeb1c4d224a59b4edb2b3bbf1a767a07485dcf
derka12 <derokabore@gmail.com>
2017-04-14 16:10:27 +0000

    add test2.c

---
+++ test2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int i1, int2) {
+	return i1 > 12 ? i1 : i2; 
+}
+
+int main(int argc, char* argc) {
+	
+	printf("Le max entre %d et %d est %d.", 10, 20, max(10, 20));
+	return EXIT_SUCESS;
+
+}
